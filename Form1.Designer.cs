5d5c10cd8dc9be13a4cece7bce7f0483f5b8199b
kertalo <koksharov-kir@yandex.ru>
2023-09-28 10:00:16 +0300

    fix 1a, 1b

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,25 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -74,12 +93,218 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(126, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 32);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(164, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 32);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(202, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(32, 32);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Navy;
+            this.button7.Location = new System.Drawing.Point(962, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(32, 32);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Blue;
+            this.button8.Location = new System.Drawing.Point(924, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(32, 32);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Teal;
+            this.button9.Location = new System.Drawing.Point(886, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(32, 32);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Aqua;
+            this.button10.Location = new System.Drawing.Point(848, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(32, 32);
+            this.button10.TabIndex = 10;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Green;
+            this.button11.Location = new System.Drawing.Point(810, 12);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(32, 32);
+            this.button11.TabIndex = 11;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Lime;
+            this.button12.Location = new System.Drawing.Point(772, 12);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(32, 32);
+            this.button12.TabIndex = 12;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Olive;
+            this.button13.Location = new System.Drawing.Point(734, 12);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(32, 32);
+            this.button13.TabIndex = 13;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Yellow;
+            this.button14.Location = new System.Drawing.Point(696, 12);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(32, 32);
+            this.button14.TabIndex = 14;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Maroon;
+            this.button15.Location = new System.Drawing.Point(658, 12);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(32, 32);
+            this.button15.TabIndex = 15;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Red;
+            this.button16.Location = new System.Drawing.Point(620, 12);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(32, 32);
+            this.button16.TabIndex = 16;
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.Purple;
+            this.button17.Location = new System.Drawing.Point(582, 12);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(32, 32);
+            this.button17.TabIndex = 17;
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.Fuchsia;
+            this.button18.Location = new System.Drawing.Point(544, 12);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(32, 32);
+            this.button18.TabIndex = 18;
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.White;
+            this.button19.Location = new System.Drawing.Point(506, 12);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(32, 32);
+            this.button19.TabIndex = 19;
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.Color.Silver;
+            this.button20.Location = new System.Drawing.Point(468, 12);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(32, 32);
+            this.button20.TabIndex = 20;
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.BackColor = System.Drawing.Color.Gray;
+            this.button21.Location = new System.Drawing.Point(430, 12);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(32, 32);
+            this.button21.TabIndex = 21;
+            this.button21.UseVisualStyleBackColor = false;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.BackColor = System.Drawing.Color.Black;
+            this.button22.Location = new System.Drawing.Point(392, 12);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(32, 32);
+            this.button22.TabIndex = 22;
+            this.button22.UseVisualStyleBackColor = false;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1006, 465);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -97,6 +322,25 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,31 +21,36 @@ namespace Paint_2._0
             Rectangle rectangle = Screen.PrimaryScreen.Bounds;
             map = new Bitmap(rectangle.Width, rectangle.Height);
             graphics = Graphics.FromImage(map);
-            Console.WriteLine(map.Width);
         }
 
         byte state = 0;
 
+        Bitmap pictureMap = new Bitmap(@"cs.jpg");
+        
         int oldX;
         int oldY;
 
         Bitmap map = new Bitmap(256, 256);
         Graphics graphics;
-        Pen pen = new Pen(Color.Black, 3);
+        Color color = Color.Black;
 
         bool isMouseDown = false;
 
         private void DrawLineBR(int x1, int y1, int x2, int y2)
         {
+            if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 ||
+                x1 >= map.Width || y1 >= map.Height ||
+                x2 >= map.Width || y2 >= map.Height)
+                return;
             int deltaX = Math.Abs(x2 - x1);
             int deltaY = Math.Abs(y2 - y1);
             int signX = x1 < x2 ? 1 : -1;
             int signY = y1 < y2 ? 1 : -1;
             int error = deltaX - deltaY;
-            map.SetPixel(x2, y2, Color.Black);
+            map.SetPixel(x2, y2, color);
             while (x1 != x2 || y1 != y2)
             {
-                map.SetPixel(x1, y1, Color.Black);
+                map.SetPixel(x1, y1, color);
                 int error2 = error * 2;
                 if (error2 > -deltaY)
                 {
@@ -59,17 +65,212 @@ namespace Paint_2._0
             }
         }
 
-        private void Fill(int x, int y, Color fillColor)
+        private void FillPicture(int x, int y, Color oldColor)
         {
             if (x < 0 || y < 0 || x >= map.Width || y >= map.Height)
                 return;
-            if (map.GetPixel(x, y) != fillColor)
+            if (map.GetPixel(x, y) != oldColor)
                 return;
-            map.SetPixel(x, y, Color.Black);
-            Fill(x + 1, y, fillColor);
-            Fill(x - 1, y, fillColor);
-            Fill(x, y + 1, fillColor);
-            Fill(x, y - 1, fillColor);
+            int startX = x;
+            while (map.GetPixel(x, y) == oldColor)
+            {
+                map.SetPixel(x, y, pictureMap.GetPixel
+                (x % pictureMap.Width, y % pictureMap.Height));
+                x++;
+                if (x >= map.Width)
+                {
+                    x--;
+                    break;
+                }
+            }
+            int rightX = x;
+            x = startX - 1;
+            if (x < 0)
+                x++;
+            while (map.GetPixel(x, y) == oldColor)
+            {
+                map.SetPixel(x, y, pictureMap.GetPixel
+                (x % pictureMap.Width, y % pictureMap.Height));
+                x--;
+                if (x < 0)
+                {
+                    x++;
+                    break;
+                }
+            }
+            for (int i = x + 1; i < rightX; i++)
+            {
+                FillPicture(i, y - 1, oldColor);
+                FillPicture(i, y + 1, oldColor);
+            }
+        }
+
+        private void FillQuick(int x, int y, Color oldColor)
+        {
+            if (x < 0 || y < 0 || x >= map.Width || y >= map.Height)
+                return;
+            while (map.GetPixel(x, y) == oldColor)
+                x++;
+            byte rotation = 2;
+            List<Point> points = new List<Point>();
+            while (true)
+            {
+                if (points.Contains(new Point(x, y)))
+                    break;
+                points.Add(new Point(x, y));
+                map.SetPixel(x, y, color);
+                if (rotation == 2)
+                {
+                    if (map.GetPixel(x - 1, y) != oldColor)
+                    {
+                        x -= 1;
+                        rotation = 3;
+                    }
+                    else if (map.GetPixel(x - 1, y + 1) != oldColor)
+                    {
+                        x -= 1;
+                        y += 1;
+                    }
+                    else if (map.GetPixel(x, y + 1) != oldColor)
+                    {
+                        y += 1;
+                    }
+                    else if (map.GetPixel(x + 1, y + 1) != oldColor)
+                    {
+                        x += 1;
+                        y += 1;
+                    }
+                    else if (map.GetPixel(x + 1, y) != oldColor)
+                    {
+                        x += 1;
+                    }
+                    //else
+                        //rotation = 0;
+                }
+                else if (rotation == 3)
+                {
+                    if (map.GetPixel(x, y - 1) != oldColor)
+                    {
+                        y -= 1;
+                        rotation = 0;
+                    }
+                    else if (map.GetPixel(x - 1, y - 1) != oldColor)
+                    {
+                        x -= 1;
+                        y -= 1;
+                    }
+                    else if (map.GetPixel(x - 1, y) != oldColor)
+                    {
+                        x -= 1;
+                    }
+                    else if (map.GetPixel(x - 1, y + 1) != oldColor)
+                    {
+                        x -= 1;
+                        y += 1;
+                    }
+                    else if (map.GetPixel(x, y + 1) != oldColor)
+                    {
+                        y += 1;
+                    }
+                    //else
+                    //    rotation = 1;
+                }
+                else if (rotation == 0)
+                {
+                    if (map.GetPixel(x + 1, y) != oldColor)
+                    {
+                        x += 1;
+                        rotation = 1;
+                    }
+                    else if (map.GetPixel(x + 1, y - 1) != oldColor)
+                    {
+                        x += 1;
+                        y -= 1;
+                    }
+                    else if (map.GetPixel(x, y - 1) != oldColor)
+                    {
+                        y -= 1;
+                    }
+                    else if (map.GetPixel(x - 1, y - 1) != oldColor)
+                    {
+                        x -= 1;
+                        y -= 1;
+                    }
+                    else if (map.GetPixel(x - 1, y) != oldColor)
+                    {
+                        x -= 1;
+                    }
+                    //else
+                    //    rotation = 2;
+                }
+                else if (rotation == 1)
+                {
+                    if (map.GetPixel(x, y + 1) != oldColor)
+                    {
+                        y += 1;
+                        rotation = 2;
+                    }
+                    else if (map.GetPixel(x + 1, y + 1) != oldColor)
+                    {
+                        x += 1;
+                        y += 1;
+                    }
+                    else if (map.GetPixel(x + 1, y) != oldColor)
+                    {
+                        x += 1;
+                    }
+                    else if (map.GetPixel(x + 1, y - 1) != oldColor)
+                    {
+                        x += 1;
+                        y -= 1;
+                    }
+                    else if (map.GetPixel(x, y - 1) != oldColor)
+                    {
+                        y -= 1;
+                    }
+                    //else
+                    //    rotation = 3;
+                }
+            }
+            int firstY = y;
+        }
+
+        private void Fill(int x, int y, Color oldColor)
+        {
+            if (x < 0 || y < 0 || x >= map.Width || y >= map.Height)
+                return;
+            if (map.GetPixel(x, y) != oldColor)
+                return;
+            int startX = x;
+            while (map.GetPixel(x, y) == oldColor)
+            {
+                map.SetPixel(x, y, color);
+                x++;
+                if (x >= map.Width)
+                {
+                    x--;
+                    break;
+                }
+            }
+            int rightX = x;
+            x = startX - 1;
+            if (x < 0)
+                x++;
+            while (map.GetPixel(x, y) == oldColor)
+            {
+                map.SetPixel(x, y, color);
+                x--;
+                if (x < 0)
+                {
+                    x++;
+                    break;
+                }
+            }
+            for (int i = x + 1; i < rightX; i++)
+            {
+                Fill(i, y - 1, oldColor);
+                Fill(i, y + 1, oldColor);
+            }
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
@@ -77,6 +278,18 @@ namespace Paint_2._0
             if (state == 2)
             {
                 Fill(e.X, e.Y, map.GetPixel(e.X, e.Y));
+                pictureBox1.Image = map;
+                return;
+            }
+            if (state == 3)
+            {
+                FillPicture(e.X, e.Y, map.GetPixel(e.X, e.Y));
+                pictureBox1.Image = map;
+                return;
+            }
+            if (state == 4)
+            {
+                FillQuick(e.X, e.Y, map.GetPixel(e.X, e.Y));
                 pictureBox1.Image = map;
                 return;
             }
@@ -117,6 +330,104 @@ namespace Paint_2._0
         private void button3_Click(object sender, EventArgs e)
         {
             state = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            state = 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            state = 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < map.Width; i++)
+                for (int j = 0; j < map.Height; j++)
+                    map.SetPixel(i, j, Color.White);
+            pictureBox1.Image = map;
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            color = Color.Black;
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            color = Color.Gray;
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            color = Color.Silver;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            color = Color.White;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            color = Color.Fuchsia;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            color = Color.Pink; //
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            color = Color.Red;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            color = Color.Maroon;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            color = Color.Yellow;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            color = Color.Olive;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            color = Color.Lime;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            color = Color.Green;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            color = Color.Aqua;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            color = Color.Teal;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            color = Color.Blue;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            color = Color.Navy;
         }
     }
 }
